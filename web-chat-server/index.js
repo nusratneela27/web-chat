36f38b5434132b131f98a6201046b553b5904486
neela <nusratneela27@gmail.com>
2025-04-12 21:02:45 +0600

    server upadate

--- web-chat-server/index.js
+++ web-chat-server/index.js
@@ -14,7 +14,11 @@ const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  }));
+// app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
